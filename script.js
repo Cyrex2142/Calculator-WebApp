a001a7831afb0ca7a0c79e077eb843d0c07ceef2
Cyrex2142 <93499531+Cyrex2142@users.noreply.github.com>
2022-04-25 22:39:26 +0930

    all functionality working if a little sloppy

--- script.js
+++ script.js
@@ -1,35 +1,103 @@
 const calculatorScreen = document.getElementById('screen');
 const buttonPressed = document.querySelectorAll('.calcInputButtons');
-var numberStore ;
+var numberStore = 0;
+var numberStoreB = 0;
+var operatorStore = '';
+var used = 0;
     buttonPressed.forEach(element => {
         element.addEventListener('click', function handleClick(event){
-            switch (element.id) {
-                case 'C':
-                    calculatorScreen.innerText = '';
-                    break;
-                case '/':
-                    numberStore = calculatorScreen.innerText;
-                    calculatorScreen.innerText = element.id;
-                    console.log(numberStore);
-                    break;
-                case '*':
-                    numberStore = calculatorScreen.innerText;
-                    calculatorScreen.innerText = element.id;
-                    console.log(numberStore);
-                    break;
-                case '+':
-                    numberStore = calculatorScreen.innerText;
-                    calculatorScreen.innerText = element.id;
-                    console.log(numberStore);
-                    break;
-                case '-':
-                    numberStore = calculatorScreen.innerText;
-                    calculatorScreen.innerText = element.id;
-                    console.log(numberStore);
-                    break;
-                default:
-                    calculatorScreen.innerText += element.id;
-                    break;
+            if(numberStore == 0) {
+                switch (element.id) {
+                    case 'C':
+                        calculatorScreen.innerText = '';
+                        numberStore = 0;
+                        numberStoreB = 0;
+                        operatorStore = '';
+                        used = 0;
+                        break;
+                    case '/':
+                        numberStore = parseInt(calculatorScreen.innerText);
+                        calculatorScreen.innerText = '';    
+                        operatorStore = element.id;
+                        break;
+                    case '*':
+                        numberStore = parseInt(calculatorScreen.innerText);
+                        calculatorScreen.innerText = '';
+                        operatorStore = element.id;
+                        break;
+                    case '+':
+                        numberStore = parseInt(calculatorScreen.innerText);
+                        calculatorScreen.innerText = '';
+                        operatorStore = element.id;
+                        break;
+                    case '-':
+                        numberStore = parseInt(calculatorScreen.innerText);
+                        calculatorScreen.innerText = '';
+                        operatorStore = element.id;
+                        break;
+                    
+                    default:
+                        if (used != 0){
+                            calculatorScreen.innerText = '';
+                            calculatorScreen.innerText += element.id;
+                            used = 0;
+                        } else {
+                            calculatorScreen.innerText += element.id;
+                        }
+                        break;
+                }
+            } else if (numberStore != 0) {
+                var result = 0;
+                switch (element.id) {
+                    case 'C':
+                        calculatorScreen.innerText = '';
+                        numberStore = 0;
+                        numberStoreB = 0;
+                        operatorStore = '';
+                        used = 0;
+                        break;
+                    case '=':
+                        used = 1;
+                        numberStoreB = parseInt(calculatorScreen.innerText);
+                        calculatorScreen.innerText = '';
+                        switch (operatorStore){
+                            case '/':
+                                result = (numberStore/numberStoreB);
+                                calculatorScreen.innerText = result;
+                                numberStore = 0;
+                                numberStoreB = 0;
+                                break;
+                            case '*':
+                                result = numberStore*numberStoreB;
+                                calculatorScreen.innerText = result;
+                                numberStore = 0;
+                                numberStoreB = 0;
+                                break;
+                            case '+':
+                                result = (numberStore + numberStoreB);
+                                calculatorScreen.innerText = result;
+                                numberStore = 0;
+                                numberStoreB = 0;
+                                break;
+                            case '-':
+                                result = numberStore-numberStoreB;
+                                calculatorScreen.innerText = result;
+                                numberStore = 0;
+                                numberStoreB = 0;
+                                break;
+                        }
+                    case '/':
+                        break;
+                    case '*':;
+                        break;
+                    case '+':
+                        break;
+                    case '-':
+                        break;
+                    default:
+                        calculatorScreen.innerText += element.id;
+                        break;
+                }
             }
             
         });
